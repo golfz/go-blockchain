5e24dd19497b958f122824403bd51d4d8d9cd1e8
Surattikorn Chumkaew <surattikorn@gmail.com>
2018-08-17 23:15:41 +0700

    add respondWithJSON func

--- main.go
+++ main.go
@@ -143,6 +143,17 @@ func handleWriteBlock(w http.ResponseWriter, r *http.Request) {
 
 }
 
+func respondWithJSON(w http.ResponseWriter, r *http.Request, code int, payload interface{}) {
+	response, err := json.MarshalIndent(payload, "", "  ")
+	if err != nil {
+		w.WriteHeader(http.StatusInternalServerError)
+		w.Write([]byte("HTTP 500: Internal Server Error"))
+		return
+	}
+	w.WriteHeader(code)
+	w.Write(response)
+}
+
 func main() {
 
 }
