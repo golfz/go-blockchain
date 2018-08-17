916fdb0f6af3710217c70de560ced3516c97c026
Surattikorn Chumkaew <surattikorn@gmail.com>
2018-08-17 22:45:39 +0700

    add main.go

---
+++ main.go
@@ -0,0 +1,28 @@
+package main
+
+import (
+	"crypto/sha256"
+	"encoding/hex"
+)
+
+type Block struct {
+	Index     int
+	Timestamp string
+	BPM       int
+	Hash      string
+	PrevHash  string
+}
+
+var Blockchain []Block
+
+func calculateHash(block Block) string {
+	record := string(block.Index) + block.Timestamp + string(block.BPM) + block.PrevHash
+	h := sha256.New()
+	h.Write([]byte(record))
+	hashed := h.Sum(nil)
+	return hex.EncodeToString(hashed)
+}
+
+func main() {
+
+}
